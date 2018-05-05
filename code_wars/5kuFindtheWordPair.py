3e689defc73356372c5acb02ee8c6eac7f6f70cc
LuanBoheng <314887027@qq.com>
2018-05-05 22:41:58 +1000

    list.index(x) 返回第一个元素的位置

--- code_wars/5kuFindtheWordPair.py
+++ code_wars/5kuFindtheWordPair.py
@@ -10,18 +10,32 @@ test_data = {
 }
 def input_format(x): return {'words':x[0],'target':x[1]}
 
-import unit_test
+import unit_test,debugy
+
 @unit_test.unit_test(test_data,input_format)
+@debugy.logger
 def compound_match(words, target):
     d = {}
     for i in range(len(words)):
         if words[i] in target:
-            d[words[i]] = i
+            if words[i] not in d:
+                d[words[i]] = i
     result = list(d.items())
     N = len(result)
     for i in range(N):
         for j in range(N):
             if result[i][0]+result[j][0] == target:
                 output = [result[i][1],result[j][1]]
-    words = map(lambda x:d[x], sorted(output))
-    return [words,output]
+                words = list(map(lambda x:words[x], sorted(output)))
+                return [*words,output]
+
+@unit_test.unit_test(test_data,input_format)
+@debugy.logger
+def compound_match_best(words, target):
+    for i in range(1,len(target)-1):
+        t1 = target[:i]; t2 = target[i:]
+        if (t1 in words) and (t2 in words):
+            i1 = words.index(t1); i2 = words.index(t2)
+            return  ([t1, t2] if i1<i2 else [t2, t1]) + [[i1,i2]]
+
+debugy.analysis.print_reports()
